84341c89fd2be214400a44836c2cd3658ad2b44c
sneubig <neubig.stephen@gmail.com>
2014-11-10 14:45:50 -0500

    add dependencies to app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ./vendor/underscore
+//= require ./vendor/backbone
+//= require ./vendor/handlebars
+//= require_self
+//= require_tree ./backbone/routers
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
 //= require_tree .
