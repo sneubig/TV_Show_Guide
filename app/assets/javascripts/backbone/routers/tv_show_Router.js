63cdecd8e206ff093408219ab907e0389957c4d0
sneubig <neubig.stephen@gmail.com>
2014-11-10 15:25:56 -0500

    instantiate tv_shows collections and collection view

--- app/assets/javascripts/backbone/routers/tv_show_Router.js
+++ app/assets/javascripts/backbone/routers/tv_show_Router.js
@@ -1,5 +1,10 @@
 App.Router = Backbone.Router.extend({
 	routes: {
 		'': 'index'
+	}, 
+
+	initialize: function() {
+		App.Collections.tv_shows = new App.TvShowCollection(App.TempData.tv_shows);
+		App.Views.tv_showListView = new App.TvShowListView({collection: App.Collections.tv_shows});
 	}
 })
